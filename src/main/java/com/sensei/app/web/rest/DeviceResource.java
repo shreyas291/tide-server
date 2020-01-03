50184db603624b77e75fc81a787d89da724fcfb3
Deepak Samal <deepak.samal@sen-sei.in>
2020-01-03 10:43:04 +0530

    updated pom

--- src/main/java/com/sensei/app/web/rest/DeviceResource.java
+++ src/main/java/com/sensei/app/web/rest/DeviceResource.java
@@ -1,26 +1,38 @@
 package com.sensei.app.web.rest;
 
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import com.codahale.metrics.annotation.Timed;
 
-/**
- * @author HP
- *
- */
+import javax.validation.Valid;
+import java.net.URISyntaxException;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class DeviceResource {
+   // private final DeviceService deviceService;
 
-    private final Logger log = LoggerFactory.getLogger(DeviceResource.class);
-    
-    public DeviceResource() {
-    	
+//    @Autowired
+//    public DeviceResource(DeviceService deviceService) {
+//        this.deviceService = deviceService;
+//    }
+
+    /**
+     * GET /devices : get all devices.
+     *
+     * @return the ResponseEntity with status 200 (OK) and with body all users
+     */
+    @GetMapping("/hi")
+    @Timed
+    public String Sample() {
+        return "Hello";
     }
 
     /**
@@ -29,9 +41,17 @@ public class DeviceResource {
      * @param pageable the pagination information
      * @return the ResponseEntity with status 200 (OK) and with body all users
      */
-    @GetMapping("/devices")
-    @Timed
-    public String Sample() {
-        return "Hi";
-    }
+//    @GetMapping("/devices")
+//    @Timed
+//    public ResponseEntity<List<DeviceDTO>> getAllDevices(Pageable pageable) {
+//        final Page<DeviceDTO> page = deviceService.getAllManagedDevices(pageable);
+//        HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(page, "/api/devices");
+//        return new ResponseEntity<>(page.getContent(), headers, HttpStatus.OK);
+//    }
+//
+//    @PostMapping("/modifyDevice")
+//    @Timed
+//    public ResponseEntity<DeviceDTO> addDevice(@Valid @RequestBody DeviceDTO deviceDTO)  {
+//        return new ResponseEntity<>(deviceService.addDevice(deviceDTO), HttpStatus.OK);
+//    }
 }
